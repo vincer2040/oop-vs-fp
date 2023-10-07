27d4acf8cf46f7903d4ca839c61b828680144fb5
boreddad <ronnebaumfinances@gmail.com>
2023-10-07 16:34:19 -0400

    feat: tokenTypeToString

--- oop/src/token.ts
+++ oop/src/token.ts
@@ -15,3 +15,28 @@ export type Token = {
     type: TokenType;
     literal: string | null;
 };
+
+export function tokenTypeTostring(tokType: TokenType): string {
+    switch (tokType) {
+        case TokenType.Illegal:
+            return "Illegal";
+        case TokenType.Simple:
+            return "Simple";
+        case TokenType.BulkType:
+            return "BulkType";
+        case TokenType.ArrayType:
+            return "ArrayType";
+        case TokenType.Len:
+            return "Len";
+        case TokenType.Bulk:
+            return "Bulk";
+        case TokenType.RetCar:
+            return "RetCar";
+        case TokenType.NewL:
+            return "NewL";
+        case TokenType.Eof:
+            return "Eof";
+        default:
+            return "Illegal";
+    }
+}
