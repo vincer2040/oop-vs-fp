6414ed3c635bef802f3265a4dee3a2b1bda92158
boreddad <ronnebaumfinances@gmail.com>
2023-10-08 13:01:09 -0400

    feat: command runner

--- fp/src/cmd.ts
+++ fp/src/cmd.ts
@@ -1,3 +1,5 @@
+import { addBulk, addNone, addOk, addPong, createProtocolBuilder, out } from "./protocolBuilder";
+
 export enum CmdType {
     Inv,
     Ping,
@@ -23,4 +25,46 @@ export type Command = {
     type: CmdType,
     cmd: null | SetCommand | GetCommand | DelCommand;
 };
+
+export function runCmd(cmd: Command, db: Map<string, string | undefined | null>): Buffer {
+    switch (cmd.type) {
+        case CmdType.Ping: {
+            const builder = createProtocolBuilder();
+            const withPing = addPong(builder);
+            return out(withPing);
+        };
+        case CmdType.Set: {
+            const setCommand = cmd.cmd as SetCommand;
+            const key = setCommand.key;
+            const value = setCommand.value;
+            db.set(key, value);
+            const builder = createProtocolBuilder();
+            const withOk = addOk(builder);
+            return out(withOk);
+        };
+        case CmdType.Get: {
+            const getCommand = cmd.cmd as GetCommand;
+            const key = getCommand.key;
+            const val = db.get(key);
+            const builder = createProtocolBuilder();
+            if (!val) {
+                const withNone = addNone(builder);
+                return out(withNone);
+            } else {
+                const withVal = addBulk(builder, val);
+                return out(withVal);
+            }
+        }
+        case CmdType.Del: {
+            const delCommand = cmd.cmd as DelCommand;
+            const key = delCommand.key;
+            db.delete(key);
+            const builder = createProtocolBuilder();
+            const withOk = addOk(builder);
+            return out(withOk);
+        }
+        default:
+            return Buffer.from("-Invalid\r\n");
+    }
+}
 
