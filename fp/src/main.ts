8f65c4ed9c3e381a473a4b757b7545abd428aa81
boreddad <ronnebaumfinances@gmail.com>
2023-10-08 13:01:21 -0400

    feat: fp server

--- fp/src/main.ts
+++ fp/src/main.ts
@@ -1,2 +1,35 @@
+import { createServer, Socket } from "net";
+import { createLexer } from "./lexer";
+import { createParser, parse } from "./parser";
+import { evaluate } from "./evaluator";
+import { runCmd } from "./cmd";
 
-console.log("hello");
+const db: Map<string, string> = new Map();
+
+function main() {
+    const server = createServer();
+
+    server.listen(6969, "127.0.0.1", () => {
+        console.log("127.0.0.1:6969");
+    });
+
+    server.on("connection", onConnect)
+}
+
+function onConnect(socket: Socket) {
+    socket.on("data", (data: Buffer) => {
+        const lexer = createLexer(data);
+        const parser = createParser(lexer);
+        const [_, val] = parse(parser);
+        const cmd = evaluate(val);
+        const buf = runCmd(cmd, db);
+        socket.write(buf);
+    });
+
+    socket.on("end", () => {
+        socket.removeAllListeners();
+        console.log("client disconnected");
+    });
+}
+
+main();
